6d81f130914150b91626ed3bff8d2fbe205dd4f6
Malik Kekenov <kekenov.malik@gmail.com>
2014-08-10 05:49:30 +0600

    Created own template

--- myPoll/polls/views.py
+++ myPoll/polls/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-
-
+from polls.models import Poll
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
