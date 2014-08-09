e21539e3749ae54f6bee16b9cdf44ea19bfb1a17
Malik Kekenov <kekenov.malik@gmail.com>
2014-08-09 21:59:20 +0600

    First test of app polls, check fo exist link Polls

---
+++ myPoll/polls/features/polls.feature
@@ -0,0 +1,5 @@
+Feature: Publishing polls
+
+    Scenario: Exists polls link in admin page
+        When I log ini
+        And I see the link "Polls"
--- myPoll/polls/features/steps.py
+++ myPoll/polls/features/steps.py
@@ -1,1 +1,7 @@
+from lettuce import *
 
+
+@step(r'see the link "(.*)"')
+def see_the_link(step, name):
+    link = world.browser.find_element_by_link_text(name)
+    assert link, 'Not found link named %s' % name
