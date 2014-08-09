6f46d913962678366fbc81a5b980fec9e11180bd
Malik Kekenov <kekenov.malik@gmail.com>
2014-08-10 02:49:45 +0600

    test for adding poll

--- myPoll/polls/features/polls.feature
+++ myPoll/polls/features/polls.feature
@@ -3,3 +3,18 @@ Feature: Publishing polls
     Scenario: Exists polls link in admin page
         When I log in
         And I see the link "Polls"
+
+    Scenario: Add new poll
+        Given I log in
+        And I click on the link "Polls"
+        And I click on the link "Add"
+        And I click on the link "Show"
+        And I click on the link "Today"
+        And I click on the link "Now"
+        And fill the form:
+			| name                     | value  |
+			| question                 | Test ? |
+			| choice_set-0-choice_text | first  |
+			| choice_set-1-choice_text | second |
+		And I click element by name "_save"
+		Then I see the text "The poll "Test ?" was added successfully" by css-path ".success"
--- myPoll/polls/features/steps.py
+++ myPoll/polls/features/steps.py
@@ -1,6 +1,28 @@
 from lettuce import *
+from selenium.webdriver.common.keys import Keys
+
 
 @step(r'see the link "(.*)"')
 def see_the_link(step, name):
     link = world.browser.find_element_by_link_text(name)
     assert link, 'Not found link named %s' % name
+
+
+@step(r'click on the link "(.*)"')
+def click_on_the_link(step, name):
+    link = world.browser.find_element_by_link_text(name)
+    link.click()
+    assert link, 'Link named %s were no found' % name
+
+
+
+@step(r'click element by name "(.*)"')
+def click__element_by_name(step, name):
+    element = world.browser.find_element_by_name(name)
+    element.click()
+    assert element, 'Element named %s were no found' % name
+
+@step(r'see the text "(.*)" by css-path "(.*)"')
+def see_the_submit_button(step, text, css_path):
+    element = world.browser.find_element_by_css_selector(css_path)
+    assert text in element.text, 'Text no found in element %s by css-selector %s' % css_path
