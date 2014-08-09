63ee7fb059666d2f5baa2798b4da294633fd37a6
Malik Kekenov <kekenov.malik@gmail.com>
2014-08-09 20:48:02 +0600

    mini fixes

--- myPoll/myPoll/features/change_password.py
+++ myPoll/myPoll/features/change_password.py
@@ -2,17 +2,18 @@ from lettuce import *
 from lettuce.django import django_url
 from selenium.webdriver.common.keys import Keys
 
+
 @step(r'see the password form')
 def see_the_password_form(step):
-	old = step.given(r'see the form field "old_password"')
-	new1 = step.given(r'see the form field "new_password1"')
-	new2 = step.given(r'see the form field "new_password2"')
-	submit_button = step.given('see the element by css-path ".submit-row input"')
-	assert old and new1 and new1 and submit_button, "This is not passord frm"
+    old = step.given(r'see the form field "old_password"')
+    new1 = step.given(r'see the form field "new_password1"')
+    new2 = step.given(r'see the form field "new_password2"')
+    submit_button = step.given('see the element by css-path ".submit-row input"')
+    assert old and new1 and new1 and submit_button, "This is not password form"
 
 
 @step(r'open password change page')
 def open_password_change_page(step):
-	log_in = step.given('log in')
-	pass_change_page = step.given('click on the link "Change password"')
-	assert log_in and pass_change_page, 'can not open pass change page'
+    log_in = step.given('log in')
+    pass_change_page = step.given('click on the link "Change password"')
+    assert log_in and pass_change_page, 'can not open pass change page'
