759a15f37a8c8aab7f12c641d8bb1fcb0fae53af
Malik Kekenov <kekenov.malik@gmail.com>
2014-08-09 20:32:55 +0600

    Browser wil restart each scenario

--- myPoll/terrain.py
+++ myPoll/terrain.py
@@ -2,9 +2,14 @@ from lettuce import before, after, world
 from selenium import webdriver
 
 
-@before.harvest
+@before.each_scenario
 def prepare_browser_driver(variables):
     world.browser = webdriver.Firefox()
+
+
+@after.each_scenario
+def destroy_browser(results):
+    world.browser.close()
 
 
 '''@after.harvest
